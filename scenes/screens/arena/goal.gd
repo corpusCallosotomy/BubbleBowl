9bfcb13b0961d2431ba0308cdb85e29eb2386e00
MMillion-vmat-c <max.fowlkes@gmail.com>
2025-01-25 17:06:52 -0500

    quickfix

--- scenes/screens/arena/goal.gd
+++ scenes/screens/arena/goal.gd
@@ -5,7 +5,7 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if !isPlayer1Goal:
-		$GoalSprite.scale.x=-1
+		$GoalSprite.scale=Vector2(1.5, 1.8)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
