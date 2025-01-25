32b28a425c3c50f29818c02e53922a72d3f3bd32
MMillion-vmat-c <max.fowlkes@gmail.com>
2025-01-25 14:42:22 -0500

    major updates

--- scenes/bubbles/Bubble.gd
+++ scenes/bubbles/Bubble.gd
@@ -1,12 +1,19 @@
 extends RigidBody2D
 
 @export var collisionShape: CollisionShape2D
-
+#@export var bounceForce: float = 10.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Bubble") or body.is_in_group("Environment"):
+		#apply_central_impulse(body.get_normal()*bounceForce)
+		pass
+		
--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -2,9 +2,9 @@ extends CharacterBody2D
 
 @export var isPlayer1: bool = true
 @export var pushForce: float = 800.0
-@export var knockbackForce: float = 420.0
+@export var knockbackForce: float = 1.2
 const SPEED: float = 30.0
-const MAXSPEED: float = 90.0
+const MAXSPEED: float = 1000.0
 const ACCELERATION: float = 1000.0
 const DECELERATION: float = 1000.0
 
@@ -12,9 +12,12 @@ const DECELERATION: float = 1000.0
 
 func _physics_process(delta):
 	var collision_info = move_and_collide(velocity * delta)
-	if collision_info and !collision_info.is_in_group("Player"):
-		velocity += velocity.bounce(collision_info.get_normal().normalized()*knockbackForce)
-		collision_info.get_collider().apply_central_impulse(-collision_info.get_normal()*pushForce)
+	if collision_info:
+		velocity += velocity.bounce(collision_info.get_normal().normalized()) * knockbackForce
+		if !collision_info.get_collider().is_in_group("Player") and !collision_info.get_collider().is_in_group("Environment"):
+			
+			collision_info.get_collider().apply_central_impulse(-collision_info.get_normal()*pushForce)
+			#print(collision_info.get_collider())
 		
 	var direction: Vector2 = Vector2.ZERO
 	if isPlayer1:
@@ -23,13 +26,14 @@ func _physics_process(delta):
 		direction = Input.get_vector("P2_left", "P2_right", "P2_up", "P2_down")
 	velocity += direction * SPEED
 	
+	
 	#for collisionAmount in get_slide_collision_count():
 		#var collision = get_slide_collision(collisionAmount)
 		#if collision.get_collider() is RigidBody2D:
 			#collision.get_collider().apply_central_impulse(-collision.get_normal()*pushForce)
-			
+	
+	velocity = velocity.limit_length(MAXSPEED)
+	
 	velocity = velocity.move_toward(Vector2.ZERO, DECELERATION * delta)
 	
 	#move_and_slide()
-
-
