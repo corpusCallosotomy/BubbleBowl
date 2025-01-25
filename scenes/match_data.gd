bc95a15943920e1d87fc4df8ff71b0a08525d436
Joel <joel.joshi@gmail.com>
2025-01-25 16:40:06 -0500

    consolidated GameManager behavior to match_data script, made appropriate changes

--- scenes/match_data.gd
+++ scenes/match_data.gd
@@ -1,15 +1,36 @@
 extends Node
 
+
+
+var stage = null #what stage we're on. 
+
+var scores  = [0,0] # holds the scores for each team.
+var players =[] # holds objects that represent players
+var bubbles =[] # holds objects for all bubbles on screen
+
 var spawnRate: float = 3.0
 
-var score_p1: int = 0
-var score_p2: int = 0
+
+
+
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta: float) -> void:
 	pass
+	
+#spawn a default(?) bubble somewhere on the board
+func spawnBubble(where: Vector2) -> void:
+	pass
+	
+func addBubble(bubble) -> void:
+	bubbles.append(bubble)
+	
+#increase the score for a team, based on a size of the incoming bubble
+func Score(team: int,size: int) -> void:
+	scores[0] +=size
+	
