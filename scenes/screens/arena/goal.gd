3eac630fd7b11076807e838a1157abcb129e1f46
MMillion-vmat-c <max.fowlkes@gmail.com>
2025-01-26 13:36:50 -0500

    VFX and art implemented

--- scenes/screens/arena/goal.gd
+++ scenes/screens/arena/goal.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 @export var isPlayer1Goal: bool = true
-
+var confettiParticleScene = preload("res://VFX/confetti2.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if !isPlayer1Goal:
@@ -16,6 +16,7 @@ func _process(_delta):
 func _on_body_entered(body):
 	#print(body)
 	if body.is_in_group("Bubble"):
+		makeConfetti()
 		body.killBubble()
 		var scoreToAdd = body.bubbleValue
 		if isPlayer1Goal:
@@ -27,3 +28,8 @@ func _on_body_entered(body):
 			$AudioStreamPlayer2D.pitch_scale = 0.9 + (scoreToAdd/10.0)
 
 			$AudioStreamPlayer2D.play()
+
+func makeConfetti():
+	var confettiInstance = confettiParticleScene.instantiate()
+	add_sibling.call_deferred(confettiInstance)
+	confettiInstance.position=self.position
